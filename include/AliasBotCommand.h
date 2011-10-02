fcb96cc25a0a3ae8fe6b259914763e6dcb08ac92
vcspth <c.spinrath@gmx.com>
2011-10-02 15:09:42 +0200

    StateBotCommand ( afk, re ) implementiert AliasBotCommand eingeführt, um command alias's zu ermöglichen

---
+++ include/AliasBotCommand.h
@@ -0,0 +1,18 @@
+#ifndef ALIASBOTCOMMAND_H
+#define ALIASBOTCOMMAND_H
+
+#include "BotCommand.h"
+
+class AliasBotCommand : public BotCommand
+{
+public:
+    AliasBotCommand(std::string argsprefix, std::string argspostfix, BotCommand *command);
+    bool invoke(const JID& user, const std::string& args, std::string *response) const;
+
+private:
+    BotCommand *_command;
+    std::string _prefix;
+    std::string _postfix;
+};
+
+#endif
---
+++ src/AliasBotCommand.cpp
@@ -0,0 +1,13 @@
+#include "AliasBotCommand.h"
+
+AliasBotCommand::AliasBotCommand(std::string prefix, std::string postfix, BotCommand *command)
+{
+    this->_command = command;
+    this->_prefix = prefix;
+    this->_postfix = postfix;
+}
+
+bool AliasBotCommand::invoke(const JID& user, const std::string& args, std::string *response) const
+{
+    return this->_command->invoke(user, this->_prefix + args + this->_postfix, response);
+}
