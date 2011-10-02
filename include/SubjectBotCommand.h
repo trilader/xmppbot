1f6895d5c8db27d13c4478234f07b5573d9fa6d9
vcspth <c.spinrath@gmx.com>
2011-10-02 02:04:34 +0200

    SubjectBotCommand implementiert; TODO: Debug Ausgaben

--- include/SubjectBotCommand.h
+++ include/SubjectBotCommand.h
@@ -3,15 +3,24 @@
 
 #include "BotCommand.h"
 #include "gloox/mucroom.h"
+#include "boost/algorithm/string/find.hpp"
+#include "boost/algorithm/string.hpp"
+#include "boost/date_time/gregorian/gregorian.hpp"
 
 class SubjectBotCommand : public BotCommand
 {
 public:
-    SubjectBotCommand(MUCRoom *room);
+    SubjectBotCommand(MUCRoom *room, std::string adminpw);
     void invoke(const JID& user, const std::string& args) const;
+    void setEvent(std::string name, std::string datestr);
 
 private:
+    bool parseArgs(const std::string& args, std::string *adminpw, std::string *subject) const;
+
     MUCRoom *_room;
+    std::string _adminpw;
+    std::string _eventname;
+    boost::gregorian::date _eventdate;
 };
 
 #endif
