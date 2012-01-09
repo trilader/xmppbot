581ce3ccb79f583ef8006592cf1537232b4b845a
Daniel Schulte <daniel@schultes-home.de>
2012-01-09 22:11:00 +0100

    Updated the .gitignore to ignore temporary files created by vi(m). Modified the Log class to padd dates and times when logging e.g. 2 -> 02.

--- src/Log.cpp
+++ src/Log.cpp
@@ -9,14 +9,38 @@ Log& Log::operator<<(const std::string& msg)
 void Log::assignFormatDateTime(StringFormat* _format)
 {
     boost::gregorian::date today = boost::gregorian::day_clock::local_day();
+    boost::gregorian::date::ymd_type ymd = today.year_month_day();
     boost::posix_time::ptime t = boost::posix_time::second_clock::local_time();
 
-    _format->assign("Y",today.year());
-    _format->assign("M",today.month());
-    _format->assign("D",today.day());
+    std::stringstream str;
+    str.width(2);
+    str.fill('0');
 
-    _format->assign("h",t.time_of_day().hours());
-    _format->assign("m",t.time_of_day().minutes());
-    _format->assign("s",t.time_of_day().seconds());
+    long hours = t.time_of_day().hours();
+    long minutes = t.time_of_day().minutes();
+    long seconds = t.time_of_day().seconds();
+    long month = ymd.month;
+
+    _format->assign("Y",ymd.year);
+    
+    str<<month; // store the month in a long first to get the number instead of the name.
+    _format->assign("M",str.str());
+    str.str("");
+
+    str<<ymd.day;
+    _format->assign("D",str.str());
+    str.str("");
+
+    str<<hours;
+    _format->assign("h",str.str());
+    str.str("");
+
+    str<<minutes;
+    _format->assign("m",str.str());
+    str.str("");
+
+    str<<seconds;
+    _format->assign("s",str.str());
+    str.str("");
 }
 boost::unordered_map<std::string, Log* > Log::logMap;
